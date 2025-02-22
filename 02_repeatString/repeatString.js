8ce922977c45aef8934edda1f4d9a281d20c1569
Biozidtonoy <150583477+Biozidtonoy@users.noreply.github.com>
2025-02-23 00:30:37 +0600

    solved problem1 and problem2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let strs = "" ;
+    for(let i = 0 ; i < num ; i++){
+        strs+=str;
+    }
+    return strs;
 };
+console.log(repeatString("hey",3));
+
 
 // Do not edit below this line
 module.exports = repeatString;
